633723ce9b030d5842a6b451770596e51c7a0572
CharliedoVale <carvalhovalecarlos@gmail.com>
2026-04-30 10:08:21 +0100

    Adiciona o meu Google Tag Manager GTM ID.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -101,7 +101,15 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={locale} className={inter.variable} suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <a
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PM93WKDD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-brand-yellow focus:text-brand-dark focus:font-bold focus:rounded-lg"
         >
@@ -113,6 +121,13 @@ export default async function LocaleLayout({ children, params }: Props) {
           <ChatWidget />
           <WhatsAppButton />
         </NextIntlClientProvider>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PM93WKDD');`,
+          }}
+        />
         <Script
           id="metricool-tracker"
           strategy="afterInteractive"
